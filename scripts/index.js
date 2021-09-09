470704ec52a5a20aaf37fcb819e02d45ed177b95
zabolotskiy_av <blagpro@yandex.ru>
2021-09-10 02:45:43 +0500

    fix: исправить пути к картинкам

--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,27 @@
 const initialCards = [
   {
     name: 'Айгир',
-    link: '../images/aigir.jpg'
+    link: 'images/aigir.jpg'
   },
   {
     name: 'Водопад Атыш',
-    link: '../images/atish.jpg'
+    link: 'images/atish.jpg'
   },
   {
     name: 'Голубое озеро',
-    link: '../images/goluboe-ozero.jpg'
+    link: 'images/goluboe-ozero.jpg'
   },
   {
     name: 'Иремель',
-    link: '../images/iremel.jpg'
+    link: 'images/iremel.jpg'
   },
   {
     name: 'Красный ключ',
-    link: '../images/krasniy-klych.jpg'
+    link: 'images/krasniy-klych.jpg'
   },
   {
     name: 'Шиханы',
-    link: '../images/shikhani.jpg'
+    link: 'images/shikhani.jpg'
   }
 ];
 
